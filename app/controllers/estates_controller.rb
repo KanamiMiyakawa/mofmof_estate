6c07aed7b74f42db648b8895735c594bf0d03f6b
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-04 07:56:56 +0000

    update estates_controller

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -1,74 +1,49 @@
 class EstatesController < ApplicationController
   before_action :set_estate, only: [:show, :edit, :update, :destroy]
 
-  # GET /estates
-  # GET /estates.json
   def index
     @estates = Estate.all
   end
 
-  # GET /estates/1
-  # GET /estates/1.json
   def show
   end
 
-  # GET /estates/new
   def new
     @estate = Estate.new
   end
 
-  # GET /estates/1/edit
   def edit
   end
 
-  # POST /estates
-  # POST /estates.json
   def create
     @estate = Estate.new(estate_params)
-
-    respond_to do |format|
-      if @estate.save
-        format.html { redirect_to @estate, notice: 'Estate was successfully created.' }
-        format.json { render :show, status: :created, location: @estate }
-      else
-        format.html { render :new }
-        format.json { render json: @estate.errors, status: :unprocessable_entity }
-      end
+    if @estate.save
+      redirect_to @estate, notice: '物件情報が作成されました'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /estates/1
-  # PATCH/PUT /estates/1.json
   def update
-    respond_to do |format|
-      if @estate.update(estate_params)
-        format.html { redirect_to @estate, notice: 'Estate was successfully updated.' }
-        format.json { render :show, status: :ok, location: @estate }
-      else
-        format.html { render :edit }
-        format.json { render json: @estate.errors, status: :unprocessable_entity }
-      end
+    if @estate.update(estate_params)
+      redirect_to @estate, notice: '物件情報が更新されました'
+    else
+      render :edit
     end
   end
 
-  # DELETE /estates/1
-  # DELETE /estates/1.json
   def destroy
     @estate.destroy
-    respond_to do |format|
-      format.html { redirect_to estates_url, notice: 'Estate was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to estates_url, notice: '物件情報を削除しました'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_estate
-      @estate = Estate.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def estate_params
-      params.require(:estate).permit(:name, :rent, :place, :age, :other)
-    end
+  def set_estate
+    @estate = Estate.find(params[:id])
+  end
+
+  def estate_params
+    params.require(:estate).permit(:name, :rent, :place, :age, :other)
+  end
 end
